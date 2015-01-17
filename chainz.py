9fbbe994ea68881c87278431d4c3702b1ad36ee4
nlaz <nikolazaris@utexas.edu>
2015-01-17 06:41:15 -0500

    seussing like a pro

--- chainz.py
+++ chainz.py
@@ -1,10 +1,14 @@
+from __future__ import print_function
 import random
 
 dict = {}
+starting_words = []
 input_file = open('fox-in-socks.txt', 'r')
 words = input_file.read().split() #note that split without arguments splits on whitespace
 for i in range(len(words)-1):
 	pair = words[i]+' '+words[i+1]
+	if pair[0].isupper():
+		starting_words.append(pair)
 	# print pair
 	if i < len(words)-2: 
 		value = words[i+2]
@@ -15,12 +19,17 @@ for i in range(len(words)-1):
 			dict[pair].append(value)
 # print dict
 	# for word in words:
-current_pair = random.choice(dict.keys())
-print current_pair
-while current_pair in dict:
-	next_word = random.choice(dict[current_pair])
-	print '['+ current_pair +'] ->'+ next_word
-	comp = current_pair.split()
-	current_pair = comp[1]+ ' '+next_word
+for i in range(4):
+	print()
+	current_pair = random.choice(starting_words)
+	print(current_pair, end=' ')
+	while current_pair in dict:
+		next_word = random.choice(dict[current_pair])
+		print(next_word, end=' ')
+		# print '['+ current_pair +'] ->'+ next_word
+		comp = current_pair.split()
+		current_pair = comp[1]+ ' '+next_word
+		if ('?' in current_pair) or ("!" in current_pair) or ("." in current_pair):
+			break
 
 
