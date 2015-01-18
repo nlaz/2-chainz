55cbfc92432b87d6e631dedcd91f781b02edc346
nlaz <nikolazaris@utexas.edu>
2015-01-18 12:11:08 -0500

    change port

--- chainz.py
+++ chainz.py
@@ -127,8 +127,5 @@ def loadSelectors(p1, p2):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
-
-
-
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
